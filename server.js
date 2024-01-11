ec14fe7f4194e6a9c6fca002e7d59c6b8b00ca91
James Penera <jamespenera@icloud.com>
2024-01-10 16:38:53 -0800

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(
   }),
 );
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/oracle_db";
 mongoose.set('strictQuery', false);
 mongoose.connect(MONGODB_URI).then(
   () => {
