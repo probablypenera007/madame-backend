58bb1400d5ddefc7d9c63715b58b92caa4f47997
James Penera <jamespenera@icloud.com>
2024-01-12 05:58:24 -0800

    Update rateLimiter.js

--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -9,7 +9,7 @@ const postRequestForAILimiter = rateLimit({
 
 const requestLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hours
-  max: 10,
+  max: 50,
   standardHeaders: true,
   legacyHeaders: false,
 });
